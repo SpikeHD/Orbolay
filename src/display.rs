c4e44f31fa85ee57b17f376569d7ab75fecc4598
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2026-05-25 20:16:34 -0700

    fix: min and max inner size

--- src/display.rs
+++ src/display.rs
@@ -39,13 +39,16 @@ pub fn update_monitor() {
   let monitor_size = (display.width, display.height);
 
   Platform::get().with_window(None, move |w| {
+    let new_size = PhysicalSize::new(
+      (monitor_size.0 + 1) as f64,
+      (monitor_size.1 - 1) as f64,
+    );
+
     w.set_outer_position(PhysicalPosition::new(
       monitor_position.0,
       monitor_position.1,
     ));
-    w.set_min_inner_size(Some(PhysicalSize::new(
-      (monitor_size.0 + 1) as f64,
-      (monitor_size.1 - 1) as f64,
-    )));
+    w.set_min_inner_size(Some(new_size));
+    w.set_max_inner_size(Some(new_size));
   });
 }
